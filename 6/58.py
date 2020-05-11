8ed1931affb81d668b96046c8c26d6c995a20274
root <root@LAPTOP-BG0S4893.localdomain>
2020-05-11 16:12:41 +0900

    fix

--- 6/58.py
+++ 6/58.py
@@ -21,12 +21,13 @@ root = tree.getroot()
 
 dependencies = root.findall('.//dependencies[@type="collapsed-dependencies"]')
 
-# svo_dict[(id, v_name)] = {s: [(id, s_name), ...], o: [(id, o_name), ...]}
-# key:   述語のidと名前のタプル
-# value: 述語に依存する主語(s)と目的語(o)の辞書; リストに語のidと名前が入っている
-svo_dict = {}
 
 for dependence in dependencies:
+    # svo_dict[(id, v_name)] = {s: [(id, s_name), ...], o: [(id, o_name), ...]}
+    # key:   述語のidと名前のタプル
+    # value: 述語に依存する主語(s)と目的語(o)の辞書; リストに語のidと名前が入っている
+    svo_dict = {}
+    
     for dep in dependence.iter("dep"):
         dep_type = dep.attrib["type"]
         governor  = dep.find("governor")
@@ -42,16 +43,17 @@ for dependence in dependencies:
         elif dep_type == "dobj":
             svo_dict[key]["o"].append((dependent.attrib["idx"], dependent.text))
 
-for v_id_name, so_dict in svo_dict.items():
-    v_name  = v_id_name[1]
-    s_id_name_list = so_dict["s"]
-    o_id_name_list = so_dict["o"]
+    for v_id_name, so_dict in svo_dict.items():
+        v_name  = v_id_name[1]
+        s_id_name_list = so_dict["s"]
+        o_id_name_list = so_dict["o"]
 
-    # 主語述語目的語の組み合わせを全通り出力
-    # 今回はs_id_name_listやo_id_name_listが複数あることはなかった
-    for s_id_name in s_id_name_list:
-        for o_id_name in o_id_name_list:
-            print(f"{s_id_name[1]}\t{v_name}\t{o_id_name[1]}") 
+        # 主語述語目的語の組み合わせを全通り出力
+        # 今回はs_id_name_listやo_id_name_listが複数あることはなかった
+        for s_id_name in s_id_name_list:
+            for o_name in o_id_name_list:
+                print(f"{s_id_name[1]}\t{v_name}\t{o_name[1]}") 
+
         
 # 
 # 出力
